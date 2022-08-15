afd92698ee92047a088be74c725822be348f632c
JoaoNeto4 <joaoquatlo@hotmail.com>
2022-08-15 14:57:05 -0300

    Capitulo 13.4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { SelectButtonModule } from 'primeng/selectbutton';
@@ -34,6 +35,7 @@ import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.co
 
     ButtonModule,
     CalendarModule,
+    DropdownModule,
     InputTextModule,
     InputTextareaModule,
     SelectButtonModule,
