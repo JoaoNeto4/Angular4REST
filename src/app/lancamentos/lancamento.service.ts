5698e5cdf5e0c3857bac1aab553452de7ee86298
JoaoNeto4 <joaoquatlo@hotmail.com>
2022-08-24 21:04:18 -0300

    Capitulo 17.6

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -3,11 +3,11 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 export class LancamentoFiltro {
-  descricao?: string;
-  dataVencimentoInicio?: Date;
-  dataVencimentoFim?: Date;
-  pagina = 0;
-  itensPorPagina = 5;
+  descricao?: string
+  dataVencimentoInicio?: Date
+  dataVencimentoFim?: Date
+  pagina: number = 0
+  itensPorPagina: number = 5
 }
 
 @Injectable({
