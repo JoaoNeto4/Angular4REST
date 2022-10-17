654a5cb0d58a027c3f4e07485d5da137840d695e
JoaoNeto4 <joaoquatlo@hotmail.com>
2022-10-17 11:36:40 -0300

    Capitulo 23.2

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { SharedModule } from '../shared/shared.module';
+import { ChartModule } from 'primeng/chart';
+import { PanelModule } from 'primeng/panel';
 
+import { SharedModule } from './../shared/shared.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    SharedModule,
 
+    PanelModule,
+    ChartModule,
+
+    SharedModule,
     DashboardRoutingModule
   ]
 })
