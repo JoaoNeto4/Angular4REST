28a5c2ca4192e8085f0da0005b288ee71e623334
JoaoNeto4 <joaoquatlo@hotmail.com>
2022-10-18 15:57:44 -0300

    Capitulo 23.8

--- src/app/relatorios/relatorios.module.ts
+++ src/app/relatorios/relatorios.module.ts
@@ -1,15 +1,20 @@
-import { SharedModule } from './../shared/shared.module';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { CalendarModule } from 'primeng/calendar';
+
+import { SharedModule } from './../shared/shared.module';
+import { RelatorioLancamentosComponent } from './relatorio-lancamentos/relatorio-lancamentos.component';
 import { RelatoriosRoutingModule } from './relatorios-routing.module';
 
-
-
 @NgModule({
-  declarations: [],
+  declarations: [RelatorioLancamentosComponent],
   imports: [
     CommonModule,
+    FormsModule,
+
+    CalendarModule,
 
     SharedModule,
     RelatoriosRoutingModule
