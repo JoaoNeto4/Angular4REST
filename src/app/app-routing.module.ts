ba5a586ea1a310f5e63def13e8635c4720b398ab
JoaoNeto4 <joaoquatlo@hotmail.com>
2022-10-18 13:43:57 -0300

    Capitulo 23.5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
 
 const routes: Routes = [
+  { path: 'dashboard', loadChildren: () => import('../app/dashboard/dashboard.module').then(m => m.DashboardModule) },
   { path: 'lancamentos', loadChildren: () => import('../app/lancamentos/lancamentos.module').then(m => m.LancamentosModule) },
   { path: 'pessoas', loadChildren: () => import('../app/pessoas/pessoas.module').then(m => m.PessoasModule) },
-  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' }, { path: 'nao-autorizado', component: NaoAutorizadoComponent },
+
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, { path: 'nao-autorizado', component: NaoAutorizadoComponent },
   { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
   { path: '**', redirectTo: 'pagina-nao-encontrada' }
 ];
